4bfa63f7e99902675133332c218188eb5f66fb8e
jeffmikhail <mikhailcruz78@gmail.com>
2022-08-28 14:58:01 +0800

    added pictures, read more function, carousel and some stuff

---
+++ js/app.js
@@ -0,0 +1,85 @@
+// * READ MORE 1
+const dots = document.getElementById("dots");
+const moreText = document.getElementById("more");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", () => {
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		myButton.innerHTML = "Read More";
+		moreText.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		myButton.innerHTML = "Read Less";
+		moreText.style.display = "inline";
+	}
+});
+
+// * READ MORE 2
+const dots2 = document.getElementById("dots2");
+const moreText2 = document.getElementById("more2");
+const myButton2 = document.getElementById("myButton2");
+
+myButton2.addEventListener("click", () => {
+	if (dots2.style.display === "none") {
+		dots2.style.display = "inline";
+		myButton2.innerHTML = "Read More";
+		moreText2.style.display = "none";
+	} else {
+		dots2.style.display = "none";
+		myButton2.innerHTML = "Read Less";
+		moreText2.style.display = "inline";
+	}
+});
+
+// * READ MORE 3
+const dots3 = document.getElementById("dots3");
+const moreText3 = document.getElementById("more3");
+const myButton3 = document.getElementById("myButton3");
+
+myButton3.addEventListener("click", () => {
+	if (dots3.style.display === "none") {
+		dots3.style.display = "inline";
+		myButton3.innerHTML = "Read More";
+		moreText3.style.display = "none";
+	} else {
+		dots3.style.display = "none";
+		myButton3.innerHTML = "Read Less";
+		moreText3.style.display = "inline";
+	}
+});
+
+// * READ MORE 4
+const dots4 = document.getElementById("dots4");
+const moreText4 = document.getElementById("more4");
+const myButton4 = document.getElementById("myButton4");
+
+myButton4.addEventListener("click", () => {
+	if (dots4.style.display === "none") {
+		dots4.style.display = "inline";
+		myButton4.innerHTML = "Read More";
+		moreText4.style.display = "none";
+	} else {
+		dots4.style.display = "none";
+		myButton4.innerHTML = "Read Less";
+		moreText4.style.display = "inline";
+	}
+});
+
+// * READ MORE 5
+const dots5 = document.getElementById("dots5");
+const moreText5 = document.getElementById("more5");
+const myButton5 = document.getElementById("myButton5");
+
+myButton5.addEventListener("click", () => {
+	if (dots5.style.display === "none") {
+		dots5.style.display = "inline";
+		myButton5.innerHTML = "Read More";
+		moreText5.style.display = "none";
+	} else {
+		dots5.style.display = "none";
+		myButton5.innerHTML = "Read Less";
+		moreText5.style.display = "inline";
+	}
+});
+5;
